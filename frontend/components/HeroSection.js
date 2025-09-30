9fc4749e44d8093060c6a2a1214407db85642b04
Laura Chipman <laurachipman66@gmail.com>
2025-09-30 19:42:08 -0400

    fixed issue for deployment to do with typo

--- frontend/components/HeroSection.js
+++ frontend/components/HeroSection.js
@@ -4,8 +4,8 @@ import Link from 'next/link';
 import Image from 'next/image';
 import Logo from '@/public/logo.png';
 import axios from 'axios';
-import { FontAwesomeIcon } from '@fontawesome/react-fontawesome';
-import { faFacebook, faInstagram } from '@fontawesome/free-brands-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faFacebook, faInstagram } from '@fortawesome/free-brands-svg-icons';
 
 const HeroSection = () => {
     const [galleryPreview, setGalleryPreview] = useState(null);
